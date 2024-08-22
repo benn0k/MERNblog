1ebb9ead9d80b4e6d2ef14d5579cded6adb4d3c5
benn0k <rbennek.rb@gmail.com>
2024-08-22 17:02:30 -0500

    structure homepage, homepage css

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,59 @@ import "./App.css";
 
 function App() {
   return (
-    <main className="text-slate-500 p-2.5 m-auto m-0 ">
+    <main>
       <header>
-        <a href="" className="logo text-blue-800">
+        <a href="" className="logo">
           Reactbox
         </a>
+        <nav className="nav-links"></nav>
+        <nav className="login">
+          <a href="">Login</a>
+        </nav>
       </header>
-      <nav>
-        <a href="">Login</a>
-      </nav>
+      <div className="content-wrapper">
+        <div className="entry-wrap">
+          <div className="entry">
+            <img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2017/07/twin-peaks.jpg"></img>
+            <div className="entry-text-content">
+              <h2>Here's an example post</h2>
+              <p>
+                Magna ad laboris dolor velit ullamco. Eiusmod enim ut occaecat
+                duis cupidatat minim sint aute officia culpa eiusmod tempor.
+                Cillum proident id nulla amet ullamco laborum consequat enim
+                aute sunt.
+              </p>
+            </div>
+          </div>
+          <div className="entry">
+            <img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2017/07/twin-peaks.jpg"></img>
+            <div className="entry-text-content">
+              <h2>Here's an example post</h2>
+              <p>
+                Magna ad laboris dolor velit ullamco. Eiusmod enim ut occaecat
+                duis cupidatat minim sint aute officia culpa eiusmod tempor.
+                Cillum proident id nulla amet ullamco laborum consequat enim
+                aute sunt.
+              </p>
+            </div>
+          </div>
+          <div className="entry">
+            <img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2017/07/twin-peaks.jpg"></img>
+            <div className="entry-text-content">
+              <h2>Here's an example post</h2>
+              <p>
+                Magna ad laboris dolor velit ullamco. Eiusmod enim ut occaecat
+                duis cupidatat minim sint aute officia culpa eiusmod tempor.
+                Cillum proident id nulla amet ullamco laborum consequat enim
+                aute sunt.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="sidebar">
+          <img src="https://tpc.googlesyndication.com/simgad/13004029155576538860"></img>
+        </div>
+      </div>
     </main>
   );
 }
